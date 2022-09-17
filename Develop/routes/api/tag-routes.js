c61f49077dc558baac6bb8c0b0ae6b21895db569
Tyler M <tmorse2222@gmail.com>
2022-09-17 16:30:47 -0700

    Tested GET routes; Category and Product successful; Tags bug (only returns tags)

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,9 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findAll({
-      include: [{ model: Product }, { model: ProductTag }],
-    });
+    const tagData = await Tag.findAll();
     res.status(200).json(tagData);
   }
   catch (err) {
