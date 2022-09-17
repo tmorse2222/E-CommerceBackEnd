dd9a04654d02c375bed6cdee99f650cb4cc30490
Tyler M <tmorse2222@gmail.com>
2022-09-17 16:44:49 -0700

    Bug fix for GET all method in tags route

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,7 +7,9 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
     res.status(200).json(tagData);
   }
   catch (err) {
