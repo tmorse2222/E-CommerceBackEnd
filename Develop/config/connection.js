96568536436f0d5009e05c425cc77b2337d1d6d2
Tyler M <tmorse2222@gmail.com>
2022-09-18 20:39:18 -0700

    Added comments to the config folder

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,12 +1,15 @@
+// Require sequelize
 const Sequelize = require('sequelize');
+// Require dotenv
 require('dotenv').config();
-
+// create new sequelize connection
 let sequelize;
-
+// connect to JAWSDB if deployed, otherwise connect to local database
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
+    // .env file variables
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
@@ -17,5 +20,5 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
-
+// Export sequelize
 module.exports = sequelize;
