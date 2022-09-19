f4960c756d0201efb631a71fca2941106b88466c
Tyler M <tmorse2222@gmail.com>
2022-09-18 20:36:41 -0700

    Added comments to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,17 @@
+// Require express
 const express = require('express');
+// Require API routes
 const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
-
+// Variable for express
 const app = express();
+// Set up port
 const PORT = process.env.PORT || 3001;
-
+// express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// Reqire routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
