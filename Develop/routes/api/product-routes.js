c0263396b77256b4b087c7f6b680cf8c7ad5f4f6
Tyler M <tmorse2222@gmail.com>
2022-09-19 18:31:24 -0700

    Bug fix to return product instead of tags when POST request is made to products

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -43,7 +43,8 @@ router.post('/', (req, res) => {
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(productTagIdArr);
+        ProductTag.bulkCreate(productTagIdArr);
+        return res.status(200).json(product);
       }
       // if no product tags, just respond
       res.status(200).json(product);
